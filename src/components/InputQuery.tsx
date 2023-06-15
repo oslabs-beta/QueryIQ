f793c37f8e44f863f2cacbf25c8944587607581a
Khaile Tran <khailetran94@gmail.com>
2023-06-15 13:02:08 -0400

    input query added in query container

---
+++ src/components/InputQuery.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+//child of QueryContainer
+//includes input field and submit button
+//query string will be submitted into input field, send triggered by button
+//request is routed
+
+const InputQuery: React.FC = () => {
+return (
+    <>
+    <div className = "w-5/12  ">
+        <input className = "w-full" placeholder="Input Query...">
+        {/* <button>GO</button> */}
+        </input>
+    </div>
+    <button className="border border-gray-900">GO</button>
+    </>
+
+    
+)
+
+
+};
+
+
+
+
+
+
+export default InputQuery;
