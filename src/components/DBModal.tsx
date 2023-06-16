435903827c54add5f0ef05f475c50fad5b0094eb
jojecameron <jojecameron@gmail.com>
2023-06-16 15:41:07 -0700

    flow of button presses for DB connection working as intended

--- src/components/DBModal.tsx
+++ src/components/DBModal.tsx
@@ -3,10 +3,13 @@ import { useState } from "react";
 
 interface DBModalProps {
   openModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setConnection: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-// currently only renders in MainContainer if state of isModalOpen set to true artificially. Eventually will be connected to DBConnect button as intermediate state between DB connection and DB disconnection
-const DBModal: React.FC<DBModalProps> = ({ openModal }) => {
+const DBModal: React.FC<DBModalProps> = ({
+  openModal,
+  setConnection,
+}) => {
   // used to cycle between modal states, selecting a database and inputting credentials
   const [dbSelection, setdbSelection] = useState(false);
 
@@ -18,10 +21,22 @@ const DBModal: React.FC<DBModalProps> = ({ openModal }) => {
     openModal(false);
   };
 
+  // placeHolder for eventual validation of form data
+  const validateData = () => {
+    const valid = true;
+    if (valid) {
+      console.log("connected");
+      setConnection(true);
+    } else {
+      console.log("Invalid Data");
+    }
+  };
+
   const handleConnect = () => {
-    // send data to back end
-    openModal(false)
-  }
+    validateData();
+    // will send data to back end if validateData is true
+    openModal(false);
+  };
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
@@ -47,42 +62,44 @@ const DBModal: React.FC<DBModalProps> = ({ openModal }) => {
           </>
         ) : (
           <>
-            <div className="flex h-auto w-auto flex-col items-center justify-center space-y-2">
-              <label>Database Name</label>
-              <input
-                className="rounded-lg border border-gray-900 p-2"
-                placeholder="Database Name"
-              ></input>
-              <label>Database URI</label>
-              <input
-                className="rounded-lg border border-gray-900 p-2"
-                placeholder="Database URI"
-              ></input>
-              <label>Database Host</label>
-              <input
-                className="rounded-lg border border-gray-900 p-2"
-                placeholder="Database Host"
-              ></input>
-              <label>Port</label>
-              <input
-                className="rounded-lg border border-gray-900 p-2"
-                placeholder="Port"
-              ></input>
-              <div className="h-45 w-45 flex justify-center">
-                {" "}
-                <button
-                  className="m-4 rounded-lg border border-gray-900 bg-indigo-500 p-2 text-gray-900 shadow-xl hover:bg-gray-900 hover:text-indigo-500"
-                  onClick={handleConnect}
-                >
-                  Connect
-                </button>
-                <button
-                  className="m-4 rounded-lg border border-gray-900 bg-red-400 p-2 text-gray-900 shadow-xl hover:bg-gray-900 hover:text-indigo-500"
-                  onClick={handleCancel}
-                >
-                  Cancel
-                </button>
-              </div>
+            <div>
+              <form className="flex h-auto w-auto flex-col items-center justify-center space-y-2">
+                <label>Database Name</label>
+                <input
+                  className="rounded-lg border border-gray-900 p-2"
+                  placeholder="Database Name"
+                ></input>
+                <label>Database URI</label>
+                <input
+                  className="rounded-lg border border-gray-900 p-2"
+                  placeholder="Database URI"
+                ></input>
+                <label>Database Host</label>
+                <input
+                  className="rounded-lg border border-gray-900 p-2"
+                  placeholder="Database Host"
+                ></input>
+                <label>Port</label>
+                <input
+                  className="rounded-lg border border-gray-900 p-2"
+                  placeholder="Port"
+                ></input>
+                <div className="h-45 w-45 flex justify-center">
+                  {" "}
+                  <button
+                    className="m-4 rounded-lg border border-gray-900 bg-indigo-500 p-2 text-gray-900 shadow-xl hover:bg-gray-900 hover:text-indigo-500"
+                    onClick={handleConnect}
+                  >
+                    Connect
+                  </button>
+                  <button
+                    className="m-4 rounded-lg border border-gray-900 bg-red-400 p-2 text-gray-900 shadow-xl hover:bg-gray-900 hover:text-indigo-500"
+                    onClick={handleCancel}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </form>
             </div>
           </>
         )}
--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -4,24 +4,10 @@ import SideBarContainer from "./SideBarContainer";
 import { useState } from "react";
 import DBModal from "~/components/DBModal";
 
-// interface MainContainerProps {
-//     onTest: () => void;
-// }
-
 const MainContainer: React.FC = ({}) => {
-  //with useState, we are able to open the modal component "DBModal"
-  //default state is false, needs to be manually changed to true for now
-  //Eventually, when "Connect to Database" button is pushed on homepage, this will cycle isModalOpen state to TRUE
-  //if modal is open, DBModal appears over the rest of the page
-  //eventually, can escape modal with cancel button or submit button
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [connection, setConnection] = useState(false);
-
-//   const onTest = () => {
-//     console.log('test ran');
-//     isModalOpen ? setIsModalOpen(false) : setIsModalOpen(true);
-//   };
 
   return (
     <div className="flex h-full w-full flex-col bg-white md:flex-row">
@@ -29,7 +15,7 @@ const MainContainer: React.FC = ({}) => {
         <></>
       ) : (
         <>
-          <DBModal openModal={setIsModalOpen} />
+          <DBModal openModal={setIsModalOpen} setConnection={setConnection}  />
         </>
       )}
       <SideBarContainer openModal={setIsModalOpen} connection={connection} setConnection={setConnection} />
