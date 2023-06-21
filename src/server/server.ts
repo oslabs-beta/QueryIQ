24f3908f9b770bee1bfd47ca064773fcf607ed39
Connor Dillon <connor@connordillon.dev>
2023-06-20 18:42:09 -0700

    :construction: WIP

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,31 +1,50 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+const express = import("express");
+import { Application, NextFunction, Request, Response } from "express";
+
+import next from "next";
 
 const port = 3000;
 
 const app: Application = express();
 
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+nextApp
+  .prepare()
+  .then(() => {
+    app.get("*", (req: Request, res: Response) => {
+      return handle(req, res);
+    });
+  })
+  .catch((err: Error) => {
+    console.error(err.stack);
+    process.exit(1);
+  });
+
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+  console.log("/ endpoint hit");
 });
 
-app.get('/api', (req: Request, res: Response) => {
-  res.send('Hello from the API');
+app.get("/user", (req: Request, res: Response) => {
+  res.send("Hello from the API");
+  console.log("/user endpoint hit");
 });
 
 // Global error handler
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   const defaultErr = {
-    log: 'Express global error handler caught unknown middleware error',
+    log: "Express global error handler caught unknown middleware error",
     status: 500,
-    message: { err: 'An error occurred' },
+    message: { err: "An error occurred" },
   };
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(port, () => {
-  console.log('Server is listening on port 3000');
-});
+app.listen(port, () => console.log(`> Ready on http://localhost:${port}`));
