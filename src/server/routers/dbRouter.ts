810dc8b1ada9d3dadd3b5ae3a9d5fc85d50854cf
Alan Beck <kalanbeck@Skunk>
2023-06-26 11:43:30 -0400

    new files

---
+++ src/server/routers/dbRouter.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import connectionController from '../controllers/connectionController';
+import databaseController from '../controllers/databaseController';
+
+
+const router = Router();
+
+//postgresql://postgres:Codesmith23*@localhost:5432/postgres
+
+//validate DB and install pg_stats_extension
+router.post('/connect',
+  connectionController.dbConnect, 
+  connectionController.createExtension, 
+  // databaseController.allStats, 
+  (req, res) => {
+    console.log(res.locals)
+    return res.status(200).json(res.locals)
+})
+
+export default router;
