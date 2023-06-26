25ed4c64097121dba3d9fa2e48d629717dde4ffd
jojecameron <jojecameron@gmail.com>
2023-06-26 13:02:53 -0700

    set test data for query state on test db connection

--- src/containers/DashboardContainer.tsx
+++ src/containers/DashboardContainer.tsx
@@ -7,6 +7,9 @@ import PieChart2 from '~/components/Graphs/PieChart2';
 import PieChart3 from '~/components/Graphs/PieChart3';
 import type { DashboardContainerProps } from '~/types/types';
 
+
+// Will render same test graphs for query as the test db metrics at the moment, can be replaced with query graphs when implemented.
+
 const DashboardContainer: React.FC<DashboardContainerProps> = ({
   testConnected,
   activeQuery,
@@ -16,7 +19,7 @@ const DashboardContainer: React.FC<DashboardContainerProps> = ({
   return (
     <div className="flex h-full w-full flex-wrap items-center justify-around p-4">
       <div className="mb-8 mt-9 flex h-1/4 w-full flex-col items-center justify-start overflow-y-auto rounded-lg border border-gray-900 shadow-xl md:h-1/4">
-        <div className="w-full flex">
+        <div className="flex w-full">
           <span
             className={`w-6/12 border-black p-1 text-center text-indigo-300 hover:bg-indigo-800 ${
               dashboardState === 'database' ? 'bg-indigo-900' : 'bg-gray-900'
@@ -35,7 +38,7 @@ const DashboardContainer: React.FC<DashboardContainerProps> = ({
           </span>
         </div>
         {dashboardState === 'database' ? (
-          <div className="flex w-full h-full md:h-96 flex-col overflow-y-auto items-center bg-gray-800 p-4 text-indigo-300">
+          <div className="flex h-full w-full flex-col items-center overflow-y-auto bg-gray-800 p-4 text-indigo-300 md:h-96">
             {!testConnected ? (
               <></>
             ) : (
@@ -50,8 +53,22 @@ const DashboardContainer: React.FC<DashboardContainerProps> = ({
             )}
           </div>
         ) : dashboardState === 'query' ? (
-          <div className="h-24 w-full overflow-y-auto bg-gray-800 p-4 text-left text-indigo-300">
-            {activeQuery.query}
+          <div className="h-96 w-full overflow-y-auto bg-gray-800 p-4 text-left text-indigo-300">
+            <div className="h-24 w-full overflow-y-auto rounded-lg border-black bg-gray-600 p-4 text-left text-indigo-300">
+              {activeQuery.query}
+            </div>
+            {!testConnected ? (
+              <></>
+            ) : (
+              <div className="flex h-full w-full flex-col items-center bg-gray-800 p-4 text-indigo-300 md:h-96">
+                <LineChart3 />
+                <PieChart />
+                <LineChart />
+                <LineChart2 />
+                <PieChart2 />
+                <PieChart3 />
+              </div>
+            )}
           </div>
         ) : null}
       </div>
