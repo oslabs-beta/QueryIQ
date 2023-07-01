a0df0dfa363c816a94a4a0de610a6b00bf8c7080
deanbiscocho <clbiscocho@gmail.com>
2023-07-01 15:25:30 -0700

    updated header font and linked it homepage endpoint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import Image from 'next/image';
 import HamburgerMenu from '../components/HamburgerMenu';
-import { Reem_Kufi } from 'next/font/google';
-
-const reemKufi = Reem_Kufi({ subsets: ['latin'], weight: ['700'] });
+import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
     <div className="flex w-screen flex-col items-center justify-center bg-white px-4 py-1 md:flex-row md:justify-between">
       <div className="flex flex-col items-center md:flex-row">
         <div className="flex justify-center md:mx-0 md:mr-4 md:items-center md:justify-start">
-          <Image
-            src="/../public/assets/logo-full-no-bg.png"
-            alt="Logo"
-            width={64}
-            height={64}
-          />
+          <Link href="/homepage">
+            <Image
+              src="/../public/assets/logo-full-no-bg.png"
+              alt="Logo"
+              width={64}
+              height={64}
+            />
+          </Link>
         </div>
         <div className="flex flex-col items-center md:flex-row md:space-x-4">
-          <h1 style={{ fontfamily: reemKufi }}>QueryIQ</h1>
+          <Link href="/homepage" className="font-reem-kufi text-4xl">
+            QueryIQ
+          </Link>
         </div>
       </div>
       <div className="flex items-center">
---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi:wght@700&display=swap" rel="stylesheet"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
