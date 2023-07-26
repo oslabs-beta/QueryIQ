72b5f40c0cd6dd2ba820788999cc76ad276ed6ac
deanbiscocho <clbiscocho@gmail.com>
2023-07-26 12:53:59 -0700

    adjusted cards

--- landing/src/components/About.tsx
+++ landing/src/components/About.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 
 const About: React.FC = () => {
   return (
     <>
-      <main id="about" className="max-h-full w-screen min-w-full max-w-full bg-transparent">
+      <section id="about" className="flex items-center justify-center min-h-screen bg-transparent">
         <div className="container mx-auto px-4 py-8">
-          <h1 className="text-4xl font-bold text-white">About Us</h1>
+          <h1 className="text-4xl font-bold text-white text-center">About</h1>
           <p className="mt-4 text-lg text-gray-300">
             We are proud to present QueryIQ, a powerful tool that empowers developers to take control of their PostgreSQL databases. 
             Whether you are an experienced developer looking to optimize performance or a novice seeking guidance, 
@@ -16,24 +17,36 @@ const About: React.FC = () => {
             By connecting their database, QueryIQ enables developers to gain valuable insights on their database from a holistic standpoint, 
             and to receive performance data through live query execution directly to their database.
           </p>
-          <div className="flex mt-8">
-            <div className="flex-1 p-4 bg-gray-100 rounded-md shadow-md">
-              <h2 className="text-xl font-semibold text-gray-800">Card 1</h2>
+          <div className="mt-8 flex flex-wrap gap-8 justify-center bg-transparent">
+            <div className="group inline-block overflow-hidden rounded-2xl bg-gradient-to-tr from-purple-800 to-teal-400 p-1 pb-4 text-white shadow transition hover:shadow-md w-[250px] h-[380px]">
+              <h2 className="text-xl font-semibold text-gray-800">Database Health</h2>
               <p className="mt-2 text-gray-600">
                 Content for Card 1 goes here.
               </p>
+              <Image
+                src="https://cdn.discordapp.com/attachments/1115285712292565056/1126315338221506640/QuIQ_5-13.gif"
+            alt="connect to database and display overall health gif"
+            width={500}
+            height={500}
+            />
             </div>
-            <div className="flex-1 p-4 bg-gray-100 rounded-md shadow-md ml-4">
-              <h2 className="text-xl font-semibold text-gray-800">Card 2</h2>
+            <div className="w-64 h-64 p-4 bg-gray-100 rounded-md shadow-md ml-4">
+              <h2 className="text-xl font-semibold text-gray-800">Query Performance</h2>
               <p className="mt-2 text-gray-600">
                 Content for Card 2 goes here.
               </p>
+              <Image
+          src="https://cdn.discordapp.com/attachments/1115285712292565056/1126317089712517190/QuIQ_query.gif"
+          alt="individual query gif"
+          width={500}
+          height={500}
+        />
             </div>
           </div>
         </div>
 
 
-      </main>
+      </section>
     </>
   );
 };
--- landing/src/pages/index.tsx
+++ landing/src/pages/index.tsx
@@ -14,18 +14,8 @@ const LandingHome: NextPage = () => {
     <>
       <Head>
         <Header />
-        <Image
-          src="https://cdn.discordapp.com/attachments/1115285712292565056/1126315338221506640/QuIQ_5-13.gif"
-          alt="connect to database and display overall health gif"
-          width={500}
-          height={500}
-        />
-        <Image
-          src="https://cdn.discordapp.com/attachments/1115285712292565056/1126317089712517190/QuIQ_query.gif"
-          alt="individual query gif"
-          width={500}
-          height={500}
-        />
+        
+        
       </Head>
       <h1>Query IQ</h1>
       <About />
