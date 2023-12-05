64a028b6a73d80b094e00c41379f594ebeeb0caa
Connor Dillon <connor@connordillon.dev>
2023-12-04 23:13:30 -0800

    Fix stdout formatting

--- cleangraf.sh
+++ cleangraf.sh
@@ -2,7 +2,14 @@
 
 # Cleans up docker images
 
-
+echo 'Removing custom-grafana image...'
 docker images | awk '$1 == "expressqueryiq-custom-grafana" {print $3}' | xargs -I {} docker image rm {}
-yes | docker volume prune
-docker volume rm expressqueryuq_grafana-storage
+
+sleep 1
+
+echo 'Clean up unused docker volumes?'
+docker volume prune
+
+echo 'Removing grafana storage volume...'
+docker volume rm expressqueryiq_grafana-storage
+
